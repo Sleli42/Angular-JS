ff64631496d7c3ca32b78e11a8acbe6278175f2c
Lucas BAUJARD <lubaujar@e1r12p19.42.fr>
2015-10-26 16:14:31 +0100

    Mon 16:14

---
+++ scotch.io_ui_rooter/app.js
@@ -0,0 +1,26 @@
+var routerApp = angular.module('routerApp', ['ui.router']);
+
+routerApp.config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/home');
+    $stateProvider
+        // HOME STATES AND NESTED VIEWS ========================================
+        .state('home', {
+            url: '/home',
+            templateUrl: 'partial-home.html'
+        })
+        .state('.list', {
+            url: '/list',
+            templateUrl: 'partial-home-list.html',
+            controller: function($scope) {
+                $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
+            }
+        })
+        .state('.paragraph', {
+            url: '/paragraph',
+            template: 'I could sure use a drink right now.'
+        })
+        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+        .state('about', {
+            // we'll get to this in a bit
+        });
+});
