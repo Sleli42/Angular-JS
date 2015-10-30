b949c56b23faa66b5c0e1a3e298d626796606578
Lucas BAUJARD <lubaujar@e1r9p16.42.fr>
2015-10-30 14:10:57 +0100

    Fri 14:10

--- advanced_directives/server.js
+++ advanced_directives/server.js
@@ -12,12 +12,19 @@ app.use(morgan('dev'));
 
 // ROUTES ==============================
 app.get('/directive1', function(req, res) {
+	// ng-show && ng-hide
 	res.render('index_directive1.ejs');
 });
 
 app.get('/directive2', function(req, res) {
+	// ng-repeat
 	res.render('index_directive2.ejs');
 });
+
+app.get('/directive3', function(req, res) {
+	// ng-switch
+	res.render('index_directive3.ejs');
+})
 
 app.listen(port, function() {
 	console.log("server running on port: " + port);
